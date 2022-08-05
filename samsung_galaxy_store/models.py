50980d8293ba966fd502db17f735cee505810cf1
minormending <54426030+minormending@users.noreply.github.com>
2022-08-04 20:33:43 -0400

    start adding docs

--- samsung_galaxy_store/models.py
+++ samsung_galaxy_store/models.py
@@ -4,6 +4,22 @@ from typing import Any, Dict, List
 
 
 def minimize_dict(maximized: Dict[Any, Any]) -> Dict[Any, Any]:
+    """Removes keys with empty and null values from a dictionary.
+
+    Args:
+        maximized: A dictionary with possible null values.
+
+    Returns:
+        A new dictionary without keys with null or empty values.
+
+    Examples:
+        >>> minimize_dict({"a": None})
+        {}
+        >>> minimize_dict({"a": ""})
+        {}
+        >>> minimize_dict({"a": None, "b": 1, "c": ""})
+        {'b': 1}
+    """    
     return {
         key: value
         for key, value in maximized.items()
@@ -12,6 +28,23 @@ def minimize_dict(maximized: Dict[Any, Any]) -> Dict[Any, Any]:
 
 
 def serialize_datetimes(dic: Dict[str, Any]) -> Dict[str, Any]:
+    """Serializes any datetime value instances in a dictionary.
+
+    The time portion is not serialized for datetime values at midnight. 
+
+    Args:
+        dic: A dictionary with possible datetime values.
+
+    Returns:
+        A new dictionary where datetime values have been converted to strings.
+
+    Examples:
+        >>> import datetime
+        >>> serialize_datetimes({"a":datetime.datetime(2022, 7, 1)})
+        {'a': '2022-07-01'}
+        >>> serialize_datetimes({"a":datetime.datetime(2022, 7, 1, 4, 7, 0)})
+        {'a': '2022-07-01 04:07:00'}
+    """    
     output: Dict[str, Any] = {}
     for key, value in dic.items():
         if not isinstance(value, datetime):
